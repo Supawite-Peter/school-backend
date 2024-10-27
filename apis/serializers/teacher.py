40b917268363c2e26ae101daedb56d50e3d6d929
Supawite Kiatrattanakorn <supawite08@gmail.com>
2024-10-27 22:48:56 +0700

    Fix teacher serializer validation

--- apis/serializers/teacher.py
+++ apis/serializers/teacher.py
@@ -24,7 +24,10 @@ class TeacherSerializer(serializers.ModelSerializer):
 class CreateTeacherSerializer(serializers.ModelSerializer):
     school_id = serializers.ModelField(model_field=Teacher._meta.get_field("school"))
     classrooms_id = serializers.PrimaryKeyRelatedField(
-        source="classrooms", queryset=Classroom.objects.all(), many=True
+        source="classrooms",
+        queryset=Classroom.objects.all(),
+        many=True,
+        required=False,
     )
 
     class Meta:
@@ -43,11 +46,11 @@ class CreateTeacherSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError("No school with the given ID was found.")
         return value
 
-    def validate(self, attrs):
+    def validate(self, attrs, school_id=None):
         if "classrooms" in attrs:
-            school_classrooms = list(
-                School.objects.get(pk=attrs["school_id"]).classrooms.all()
-            )
+            if school_id is None:
+                school_id = attrs["school_id"]
+            school_classrooms = list(School.objects.get(pk=school_id).classrooms.all())
             for classroom_id in attrs["classrooms"]:
                 if classroom_id not in school_classrooms:
                     raise serializers.ValidationError(
@@ -62,14 +65,17 @@ class CreateTeacherSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         with transaction.atomic():
             # Get classroom
-            classrooms = validated_data.pop("classrooms")
+            classrooms = None
+            if "classrooms" in validated_data:
+                classrooms = validated_data.pop("classrooms")
             # Create new teacher first
             created_teacher = Teacher.objects.create(**validated_data)
             # Then add classrooms
-            try:
-                created_teacher.classrooms.add(*classrooms)
-            except ValidationError as e:
-                raise serializers.ValidationError({"classrooms_id": list(e)})
+            if classrooms is not None:
+                try:
+                    created_teacher.classrooms.add(*classrooms)
+                except ValidationError as e:
+                    raise serializers.ValidationError({"classrooms_id": list(e)})
             return created_teacher
 
 
@@ -93,25 +99,28 @@ class UpdateTeacherSerializer(CreateTeacherSerializer):
                             "school_id": "To update school, teacher must not registered in any classroom."
                         }
                     )
-        return super().validate(attrs)
+        return super().validate(attrs, school_id=self.context["teacher_id"])
 
     def update(self, instance, validated_data):
         with transaction.atomic():
-            print(validated_data.get("school_id", instance.school))
             # Get classroom
-            classrooms_data = validated_data.pop("classrooms")
-            classrooms = instance.classrooms
+            classrooms_data = None
+            if "classrooms" in validated_data:
+                classrooms_data = validated_data.pop("classrooms")
+                classrooms = instance.classrooms
             # Save first_name, last_name, gender, school first
             instance.first_name = validated_data.get("first_name", instance.first_name)
             instance.last_name = validated_data.get("last_name", instance.last_name)
             instance.gender = validated_data.get("gender", instance.gender)
-            school_id = validated_data.get("school_id", instance.school)
-            instance.school = School.objects.get(pk=school_id)
+            if "school_id" in validated_data:
+                school_id = validated_data.get("school_id")
+                instance.school = School.objects.get(pk=school_id)
             instance.save()
             # Then update classrooms
-            try:
-                classrooms.set(classrooms_data)
-            except ValidationError as e:
-                raise serializers.ValidationError({"classrooms_id": list(e)})
+            if classrooms_data is not None:
+                try:
+                    classrooms.set(classrooms_data)
+                except ValidationError as e:
+                    raise serializers.ValidationError({"classrooms_id": list(e)})
 
             return instance
